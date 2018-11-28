b6b27a3bc255e9377b72be32de2fcbeb340e033c
Gonda Alexandra <letnia6@gmail.com>
2018-11-28 18:34:43 +0100

    könnyű játék menüje kész, main menüben feldobja az új ablakot

--- Asztali_alkalmazás/mainFXMLController.java
+++ Asztali_alkalmazás/mainFXMLController.java
@@ -5,12 +5,17 @@
  */
 package rft;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -22,9 +27,24 @@ public class mainFXMLController implements Initializable {
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void ToSelectEasy(ActionEvent event) throws IOException{
+        
+        Stage mainStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("easygameselectFXML.fxml"));
+        Scene scene = new Scene(root);
+        mainStage.setScene(scene);
+                 
+        mainStage.show();
+    }
+    @FXML
+    private void ToSelectDifficult(ActionEvent event) throws IOException{
+        
+        Stage mainStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("difficultgameselectFXML.fxml"));
+        Scene scene = new Scene(root);
+        mainStage.setScene(scene);
+                 
+        mainStage.show();
     }
     
     @Override
