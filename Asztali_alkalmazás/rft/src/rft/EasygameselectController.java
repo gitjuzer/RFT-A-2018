7d03853e0508cbf3a17d699df11ad8c1bf3ba72d
Gonda Alexandra <letnia6@gmail.com>
2018-12-06 11:04:04 +0100

    kezdőlapról nem új ablakba ugrik fel a könnyű és nehéz játék, hanem váltja a képernyőket akasztófa fxml file-ja megcsinálva

--- Asztali_alkalmazás/rft/src/rft/EasygameselectController.java
+++ Asztali_alkalmazás/rft/src/rft/EasygameselectController.java
@@ -9,11 +9,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Alex
- */
+
 public class EasygameselectController implements Initializable {
 
     /**
