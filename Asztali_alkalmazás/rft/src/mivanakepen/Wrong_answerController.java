a5b69bf5e475759d5ce3e209d2d594cd495604e3
nincsqq <jonasreka@citromail.hu>
2018-12-07 19:11:07 +0100

    Mi van a képen játék hozzáadása a játék választáshoz.

---
+++ Asztali_alkalmazás/rft/src/mivanakepen/Wrong_answerController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mivanakepen;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Cica
+ */
+public class Wrong_answerController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    @FXML
+    private void exit(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("FXML1.fxml"));
+        
+        
+    } 
+}
