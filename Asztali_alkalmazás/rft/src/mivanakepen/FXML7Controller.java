baea9cc025eacebedbce858053026f95f36f9494
nincsqq <jonasreka@citromail.hu>
2018-12-10 23:31:54 +0100

    Mi van a képen játék update

---
+++ Asztali_alkalmazás/rft/src/mivanakepen/FXML7Controller.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mivanakepen;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Cica
+ */
+public class FXML7Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    private Stage stage;
+    @FXML
+    Button button;
+    
+    @FXML
+    private void load_eighth(ActionEvent event) throws IOException {
+        stage = (Stage) button.getScene().getWindow();
+        AnchorPane root;
+        root = (AnchorPane) FXMLLoader.load(getClass().getResource("FXML8.fxml"));
+        Scene scene = new Scene(root);
+       
+        stage.setScene(scene);
+    }
+    @FXML
+    private void wrong_answer(ActionEvent event) throws IOException {
+        FXMLLoader load = new FXMLLoader (getClass().getResource("Wrong_answer.fxml"));
+        Parent root1 = (Parent) load.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
+}
