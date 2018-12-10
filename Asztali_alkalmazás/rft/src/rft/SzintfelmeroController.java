ebfe46159a0e10f4d5181993828e15d17862c987
Fanni1010 <fannimatyassy@gmail.com>
2018-12-10 18:24:48 +0100

    Szintfelmérő befejezése

--- Asztali_alkalmazás/rft/src/rft/SzintfelmeroController.java
+++ Asztali_alkalmazás/rft/src/rft/SzintfelmeroController.java
@@ -7,15 +7,22 @@ package rft;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
 
 /**
  * FXML Controller class
@@ -39,9 +46,89 @@ private Stage stage;
        
         stage.setScene(scene);
     }
+    @FXML
+    Button ellenorzobtn;
+    
+    @FXML
+    private void ellenorzes(ActionEvent event) throws IOException {
+        
+        if(firstwordlabel.getText().equals(valasz.get(index.get(0)))) helyesvalaszokszama++;
+        if(secondwordlabel.getText().equals(valasz.get(index.get(1)))) helyesvalaszokszama++;
+        if(thirdwordlabel.getText().equals(valasz.get(index.get(2)))) helyesvalaszokszama++;
+        if(forthwordlabel.getText().equals(valasz.get(index.get(3)))) helyesvalaszokszama++;
+        if(fifthwordlabel.getText().equals(valasz.get(index.get(4)))) helyesvalaszokszama++;
+        
+    FXMLLoader load = new FXMLLoader (getClass().getResource("szintfelmeroeredmeny.fxml"));
+        Parent root1 = (Parent) load.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+        
+    } 
+   
+    @FXML
+    Label label1;
+    @FXML    
+    Label label2;
+    @FXML
+    Label label3;
+    @FXML
+    Label label4;
+    @FXML
+    Label label5;
+    @FXML
+    TextField firstwordlabel;
+    @FXML
+    TextField secondwordlabel;
+    @FXML
+    TextField thirdwordlabel;
+    @FXML
+    TextField forthwordlabel;
+    @FXML
+    TextField fifthwordlabel;
+    
+    protected static int helyesvalaszokszama=0;
+    
+    protected static ArrayList<String> valasz = new ArrayList<String>();
+        
+    protected static ArrayList<Integer> index = new ArrayList<Integer>();
+    
+    protected static ArrayList<String> kerdesek = new ArrayList<String>();
+    
+    @FXML
+    private void szintfelmeres(){
+        Random rnd = new Random();
+        
+        
+        int i=0;
+        
+        while(i < 5){
+             
+            int random = rnd.nextInt(9);
+            if(!index.contains(random)) 
+            {
+            index.add(random);
+            i++;
+            }
+            
+        }
+        label1.setText(kerdesek.get(index.get(0)));
+        label2.setText(kerdesek.get(index.get(1)));
+        label3.setText(kerdesek.get(index.get(2)));
+        label4.setText(kerdesek.get(index.get(3)));
+        label5.setText(kerdesek.get(index.get(4)));
+        i=0;
+    }
+    
+   
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        Collections.addAll(kerdesek,"macska","kutya","eszik","iszik","alszik","unalmas","piros","zöld","hétfő","barátok");
+        Collections.addAll(valasz,"cat","dog","eat","drink","sleep","borring","red","green","monday","friends");
+        szintfelmeres();
+        
     }    
     
 }
