7815ff67108331269bb21b098684ab0b14b2760f
BazsaP <43841299+BazsaP@users.noreply.github.com>
2018-12-10 02:51:45 +0100

    A matekos Be Clever játékot megcsináltam

---
+++ Asztali_alkalmazás/BeClever/src/becleverapp/Question1Controller.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package becleverapp;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author LENOVOBOOK
+ */
+public class Question1Controller implements Initializable {
+
+    public void wrongAnswerButtonPushed(ActionEvent event) throws IOException{
+        Parent wrongAnswerParent = FXMLLoader.load(getClass().getResource("LosePage.fxml"));
+        Scene LosePageScene = new Scene(wrongAnswerParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(LosePageScene);
+        window.show();
+    }
+    public void goodAnswerButtonPushed(ActionEvent event) throws IOException{
+        Parent Question2Parent = FXMLLoader.load(getClass().getResource("Question2.fxml"));
+        Scene Question2Scene = new Scene(Question2Parent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(Question2Scene);
+        window.show();
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    
+    
+}
