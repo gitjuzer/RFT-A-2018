d95dcab3c905ec340621e69d08fc272a054e8c49
Dlgeri123 <43840813+Dlgeri123@users.noreply.github.com>
2018-12-11 18:17:54 +0100

    Fejtörő/app.js 1. kérdés javítása, 2. és 3. kérdés hozzáadása

--- Weboldal/OnlineNyelvtanulas/Fejtoro/app.js
+++ Weboldal/OnlineNyelvtanulas/Fejtoro/app.js
@@ -7,18 +7,54 @@ const { render } = ReactDOM;
 let data = [
     {
         id: 0,
-        question: 'What has branches and leaves but has no bark?',
+        question: 'What has a face and two hands but no arms or legs?\n',
         answers: [
             {
                 choice: 0,
-                value: 'A library'
+                value: 'A clock'
             },
             {
                 choice: 1,
-                value: 'A tree'
+                value: 'A person'
             }
         ],
         correctAnswer: 0,
-        explanation: 'Just think about it. The library made from wood.'
+        explanation: 'Az óránál angolul a mutató = hands.'
+    },
+    {
+        id: 1,
+        question: 'What has a neck but no head?\n',
+        answers: [
+            {
+                choice: 0,
+                value: 'A person'
+            },
+            {
+                choice: 1,
+                value: 'A bottle'
+            }
+        ],
+        correctAnswer: 0,
+        explanation: 'A bottle = üveg. Az üvegnek is van nyaka, illetve feje.'
+    },
+    {
+        id: 2,
+        question: 'What gets wetter as it dries?\n',
+        answers: [
+            {
+                choice: 0,
+                value: 'A dress'
+            },
+            {
+                choice: 1,
+                value: 'A costume'
+            },
+            {
+                choice: 2,
+                value: 'A towel'
+            }
+        ],
+        correctAnswer: 0,
+        explanation: 'A towel = törölköző. A törölköző vizes lesz ha megtörölkötünk.'
     }
     ];
