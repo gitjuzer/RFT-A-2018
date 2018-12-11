8f3d52d80c3beea95f66413da6640d87eaae33a7
nincsqq <jonasreka@citromail.hu>
2018-12-11 01:27:04 +0100

    Matekos játék hozzáadása a játék választáshoz

---
+++ Asztali_alkalmazás/rft/src/matematika/Question3Controller.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matematika;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Cica
+ */
+public class Question3Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    public void wrongAnswerButtonPushed(ActionEvent event) throws IOException{
+        Parent wrongAnswerParent = FXMLLoader.load(getClass().getResource("LosePage.fxml"));
+        Scene LosePageScene = new Scene(wrongAnswerParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(LosePageScene);
+        window.show();
+    }
+    public void goodAnswerButtonPushed(ActionEvent event) throws IOException{
+        Parent Question4Parent = FXMLLoader.load(getClass().getResource("Question4.fxml"));
+        Scene Question4Scene = new Scene(Question4Parent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(Question4Scene);
+        window.show();
+    }
+    
+}
