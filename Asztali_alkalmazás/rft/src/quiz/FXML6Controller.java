44be191387e6b1909ef77b0e4f49ba5b382f6752
nincsqq <jonasreka@citromail.hu>
2018-12-11 15:23:22 +0100

    Quiz jatek hozzáadása

--- Asztali_alkalmazás/rft/src/quiz/FXML6Controller.java
+++ Asztali_alkalmazás/rft/src/quiz/FXML6Controller.java
@@ -5,9 +5,18 @@
  */
 package quiz;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,5 +32,38 @@ public class FXML6Controller implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    private Stage stage;
+    @FXML
+    Button button;
+    @FXML
+    Button button1;
     
+    @FXML
+    private void loadNext(ActionEvent event) throws IOException {
+        stage = (Stage) button.getScene().getWindow();
+        AnchorPane root;
+        root = (AnchorPane) FXMLLoader.load(getClass().getResource("FXML7.fxml"));
+        Scene scene = new Scene(root);
+       
+        stage.setScene(scene);
+    }
+
+    @FXML
+    private void loadLast(ActionEvent event) throws IOException {
+        stage = (Stage) button1.getScene().getWindow();
+        AnchorPane root;
+        root = (AnchorPane) FXMLLoader.load(getClass().getResource("FXML10.fxml"));
+        Scene scene = new Scene(root);
+       
+        stage.setScene(scene);
+    }
+
+    @FXML
+    private void wrong(ActionEvent event) throws IOException {
+        FXMLLoader load = new FXMLLoader (getClass().getResource("Wrong.fxml"));
+        Parent root1 = (Parent) load.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    }
 }
--- Asztali_alkalmazás/rft/src/quiz/FXML9Controller.java
+++ Asztali_alkalmazás/rft/src/quiz/FXML9Controller.java
@@ -5,9 +5,18 @@
  */
 package quiz;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,5 +32,19 @@ public class FXML9Controller implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    private Stage stage;
+    @FXML
+    Button button;
     
+    
+    @FXML
+    private void vissza(ActionEvent event) throws IOException {
+        stage = (Stage) button.getScene().getWindow();
+        AnchorPane root;
+        root = (AnchorPane) FXMLLoader.load(getClass().getResource("/rft/easygameselectFXML.fxml"));
+        Scene scene = new Scene(root);
+       
+        stage.setScene(scene);
+    }
+
 }
