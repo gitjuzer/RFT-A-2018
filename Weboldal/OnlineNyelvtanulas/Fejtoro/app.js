28570bb3a1ffa75ff93de3e422902f1294d0a914
Dlgeri123 <43840813+Dlgeri123@users.noreply.github.com>
2018-12-11 17:58:41 +0100

    Fejtörő/app.js 1. kérdés hozzáadása

--- Weboldal/OnlineNyelvtanulas/Fejtoro/app.js
+++ Weboldal/OnlineNyelvtanulas/Fejtoro/app.js
@@ -3,3 +3,22 @@ const { Provider, connect } = ReactRedux;
 const Component = React.Component;
 const { default : styled } = styled;
 const { render } = ReactDOM;
+
+let data = [
+    {
+        id: 0,
+        question: 'What has branches and leaves but has no bark?',
+        answers: [
+            {
+                choice: 0,
+                value: 'A library'
+            },
+            {
+                choice: 1,
+                value: 'A tree'
+            }
+        ],
+        correctAnswer: 0,
+        explanation: 'Just think about it. The library made from wood.'
+    }
+    ];
